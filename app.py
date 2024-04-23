4fea8df59433fad166a00bc043bd549683d84bbe
Ryan Kinoti <ryankinotikathurima@gmail.com>
2024-04-23 16:32:11 +0300

    server-setup-v2: completed server basics

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 import os
+import random
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return redirect('/home')
 
 
 @app.route('/home')
 def home():
-    # Retrieve the server ID from the environment variable
-    server_id = os.getenv('SERVER_ID', 'Unknown')
-    return jsonify(message=f"Hello from Server: {server_id}", status="successful")
+    no = random.randint(1, 3)
+    server_id = os.getenv('SERVER_ID', f'Server-{no}')
+    return jsonify(message=f"Hello from : {server_id}", status="successful")
 
 
 @app.route('/heartbeat')
 def heartbeat():
-    # Heartbeat endpoint to check server health
-    return '', 200
+    return 'OK', 200
 
 
 if __name__ == '__main__':
