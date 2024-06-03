b15be847779e4760fb0f9a0fb381af9db4bde865
Felicia Kyalo <feliciakyalo@gmail.com>
2024-06-03 04:24:52 +0300

    server routes

--- server/server.py
+++ server/server.py
@@ -15,3 +15,23 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
+
+@app.route('/')
+def hello_world():
+    return redirect('/home')
+
+
+@app.route('/home', methods=['GET'])
+def home():
+    logging.info(f"Server ID: {SERVER_ID} received a home request")
+    return jsonify(message=f"Hello from Server: {SERVER_ID}", status="successful"), 200
+
+
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    logging.info(f"Server ID: {SERVER_ID} received a heartbeat request")
+    return 'ISS ALL GOOD', 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
