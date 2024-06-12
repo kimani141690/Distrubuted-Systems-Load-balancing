d9838c6c57fb1a83a6579273b1d058155fbfbe64
Alex Kimani <kimanialex828@gmail.com>
2024-06-12 15:21:37 +0300

    final updates

--- load_balancer/consistent_hashing.py
+++ load_balancer/consistent_hashing.py
@@ -1,60 +1,58 @@
 import math
 
-class ConsistentHash:
+import docker
+from sortedcontainers import SortedDict
 
 
+class ConsistentHashing:
 
-        def __init__(self, slots=512, no_of_servers=3):
-            self.no_of_servers = no_of_servers
-            self.slots = slots
-            self.virtual_servers = int(math.log2(slots))
-            self.hash_ring = SortedDict()
-            self.registered_paths = {'home', 'heartbeat', 'server_status'}
-            self.init_servers()
-            self.server_hash_map = {}
+    def __init__(self, slots=512, no_of_servers=3):
+        self.no_of_servers = no_of_servers
+        self.slots = slots
+        self.virtual_servers = int(math.log2(slots))
+        self.hash_ring = SortedDict()
+        self.registered_paths = {'home', 'heartbeat', 'server_status'}
+        self.init_servers()
+        self.server_hash_map = {}
 
-        # j => is the number of virtual servers per server
-        # Hash function to map requests to slots
-        def request_hash_fn(self, i):
-            # return int(hashlib.md5(str(i).encode()).hexdigest(), 16) % self.slots
-            # value = (i + (2 * i) + 17) // 2
-            value = (i ** 2 + 2 * (i ** 2) + 17 ** 2)
-            hash_value = value % self.slots
-            return hash_value
+    # j => is the number of virtual servers per server
+    # Hash function to map requests to slots
+    def request_hash_fn(self, i):
+        # return int(hashlib.md5(str(i).encode()).hexdigest(), 16) % self.slots
+        # value = (i + (2 * i) + 17) // 2
+        value = (i ** 2 + 2 * (i ** 2) + 17 ** 2)
+        hash_value = value % self.slots
+        return hash_value
 
-    def hash_request_mapping(self, i):
-        return (i ** 2 + 2 * (i ** 2) + 17 ** 2) % self.num_slots
+    # Hash function to map virtual servers to slots
+    def virtual_hashing(self, server_id, virtual_index):
+        # key = f"{server_id}-{virtual_index}"
+        # return int(hashlib.md5(key.encode()).hexdigest(), 16) % self.slots
+        numeric_id = int(server_id[1:])
+        j = virtual_index
+        value = numeric_id + j + (2 * j) + 25
+        hash_value = value % self.slots
+        return hash_value
 
-    def hash_virtual_server(self, i, j):
-        return (i + j + 2 * j + 25) % self.num_slots
+    # Add server to the hash ring
+    def add_server_to_ring(self, server_id, hostname):
+        if server_id in [value[0] for value in self.hash_ring.values()]:
 
-        # Add server to the hash ring
+            for key, value in self.hash_ring.items():
+                if value[0] == server_id:
+                    self.hash_ring[key] = (server_id, hostname)
+                    print(f"Updated server {server_id} with hostname {hostname}")
+                    break
+            raise ValueError(f"Server ID '{server_id}' already exists in the hash ring")
 
+        else:
+            for i in range(self.virtual_servers):
+                server_hash_value = self.virtual_hashing(server_id, i)
+                self.hash_ring[server_hash_value] = (server_id, hostname)
+            self.no_of_servers += 1
+            print(f"Added server {server_id} with hostname {hostname}")
 
-def add_server_to_ring(self, server_id, hostname):
-    if server_id in [value[0] for value in self.hash_ring.values()]:
-
-        for key, value in self.hash_ring.items():
-            if value[0] == server_id:
-                self.hash_ring[key] = (server_id, hostname)
-                print(f"Updated server {server_id} with hostname {hostname}")
-                break
-        raise ValueError(f"Server ID '{server_id}' already exists in the hash ring")
-
-    else:
-        for i in range(self.virtual_servers):
-            server_hash_value = self.virtual_hashing(server_id, i)
-            self.hash_ring[server_hash_value] = (server_id, hostname)
-        self.no_of_servers += 1
-        print(f"Added server {server_id} with hostname {hostname}")
-
-    def place_server(self, server_id, virtual_id, start_slot):
-        slot = start_slot
-        while self.hash_map[slot] is not None:
-            # Linear probing
-            slot = (slot + 1) % self.num_slots
-        self.hash_map[slot] = (server_id, virtual_id)
-
+    # Remove server from the hash ring
     def remove_server_from_ring(self, hostname):
         server_id = None
         for key, value in self.hash_ring.items():
@@ -71,51 +69,21 @@ def add_server_to_ring(self, server_id, hostname):
 
         return {"message": f"Server {hostname} removed successfully", "status": "successful"}, 200
 
-            # Remove the server from the servers dictionary
-            del self.servers[server_id]
-            self.num_servers -= 1
-            print(f"Removed server {server_id}")
-            print(f"All remaining servers: {', '.join(str(x) for x in self.servers.values())}")
-
-
-def map_request_to_server(self, request_id):
-    request_hash_value = self.request_hash_fn(request_id)
-    # Find the server with the next highest hash value
-    for server_hash, server_id in self.hash_ring.items():
-        if server_hash >= request_hash_value:
-            return server_id
-    # If the request hash value is greater than all server hash values wrap around to the first server
-    return self.hash_ring.peekitem(0)[1]
+    # Get the server for a given request
+    def map_request_to_server(self, request_id):
+        request_hash_value = self.request_hash_fn(request_id)
+        # Find the server with the next highest hash value
+        for server_hash, server_id in self.hash_ring.items():
+            if server_hash >= request_hash_value:
+                return server_id
+        # If the request hash value is greater than all server hash values wrap around to the first server
+        return self.hash_ring.peekitem(0)[1]
 
     # in case a server does not respond to heartbeat and needs to be updated
-
-
-def update_servers(self, server_id, hostname):
-    self.remove_server_from_ring(server_id)
-    self.add_server_to_ring(server_id, hostname)
-    return self.hash_ring
-
-
-    def get_server(self, request_id):
-        slot = self.hash_request_mapping(request_id)
-        while self.hash_map[slot] is None:
-            # This shouldn't happen in a properly initialized map, but just in case:
-            slot = (slot + 1) % self.num_slots
-        return self.hash_map[slot]
-
-
-# Initialize the consistent hash map
-consistent_hash = ConsistentHash()
-# consistent_hash.add_server(4,"S4")
-
-consistent_hash.remove_server(2)
-print(consistent_hash.hash_map)
-
-for i in range(1, 10):
-    request_id = i + 225
-    server_info = consistent_hash.get_server(request_id)
-    print(f"Request {request_id} is handled by server container {server_info[0]}, virtual server {server_info[1]}")
-
+    def update_servers(self, server_id, hostname):
+        self.remove_server_from_ring(server_id)
+        self.add_server_to_ring(server_id, hostname)
+        return self.hash_ring
 
     # initiating default servers
     def init_servers(self):
